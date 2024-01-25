bcfa0e032fe34bf3abdfc327165d998eb8147770
utkarrshhh <utkarsh3639@gmail.com>
2024-01-26 05:07:34 +0530

    update right-click

--- src/components/RightClick.jsx
+++ src/components/RightClick.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 const RightClick = () => {
+  let box = "";
   const rightClickDisplay = (e) => {
     e.preventDefault();
-    let box = document.getElementById("right-click");
+    box = document.getElementById("right-click");
     box.style.left = e.clientX + "px";
     box.style.top = e.clientY + "px";
     box.style.display = "block";
@@ -11,15 +12,21 @@ const RightClick = () => {
 
   let body = document.body;
   body.addEventListener("contextmenu", (e) => rightClickDisplay(e));
-
+  document.addEventListener("click", () => {
+    box.style.display == "block" ? (box.style.display = "none") : "";
+  });
   return (
-    <div className="right-click" id="right-click">
-      <div className="right-click-items">items</div>
-      <div className="right-click-items">items</div>
-      <div className="right-click-items">items</div>
-      <div className="right-click-items">items</div>
-      <div className="right-click-items">items</div>
-      <div className="right-click-items">items</div>
+    <div className="right-click right-click-container" id="right-click">
+      <div className="right-click-items">New Folder</div>
+      <div className=" context-divider"></div>
+      <div className="right-click-items">Get Info</div>
+      <div className="right-click-items">Change Desktop background</div>
+      <div className=" context-divider"></div>
+      <div className="right-click-items">Use Stack</div>
+      <div className="right-click-items">Sort By</div>
+      <div className="right-click-items">Clean up</div>
+      <div className="right-click-items">Clean Up By</div>
+      <div className="right-click-items">Show View Option</div>
     </div>
   );
 };
